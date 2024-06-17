a885c3b0ad1d2744ee99577c449b7af5b36287d9
Jasiek <jasiekk9@gmail.com>
2024-06-17 02:48:42 +0200

    Add runs suppoer for EA experiments

--- ea/experiment.py
+++ ea/experiment.py
@@ -13,38 +13,40 @@ def run_experiment(config_path: str = './ea/config.json', verbose: int = 0):
     with open(config_path, 'r') as f:
         config = json.load(f)
 
-    data = pd.DataFrame(columns=['generation', 'time_elapsed', 'best_fitness'])
+    data = pd.DataFrame(columns=['run', 'generation', 'time_elapsed', 'best_fitness'])
 
     env = gym.make(config["env_name"])
     Agent.architecture = [4, 4, 2]
     Agent.ENV = env
-    Agent.SEED = config["seed"]
-    np.random.seed(config["seed"])
 
-    evolution = Evolution(
-        generation_size=config["generation_size"],
-        top_k=config["top_k"],
-        mutation_args=dict(
-            prob=config["prob"],
-            strength=config["strength"]
+    for run in range(config["n_runs"]):
+        Agent.SEED = config["seed"] + run
+        np.random.seed(config["seed"] + run)
+
+        evolution = Evolution(
+            generation_size=config["generation_size"],
+            top_k=config["top_k"],
+            mutation_args=dict(
+                prob=config["prob"],
+                strength=config["strength"]
+            )
         )
-    )
-    for generation in range(config["n_generations"]):
-        if verbose > 0:
-            print(f"starting generation {generation + 1}... ", end="")
+        for generation in range(config["n_generations"]):
+            if verbose > 0:
+                print(f"starting generation {generation + 1}... ", end="")
 
-        start = time.time_ns()
-        best, fitness = evolution.simulate_one_generation()
-        duration = time.time_ns() - start
-        fitness *= 10
-        data.loc[generation] = [generation, duration, fitness]
+            start = time.time_ns()
+            best, fitness = evolution.simulate_one_generation()
+            duration = time.time_ns() - start
+            fitness *= 10
+            data.loc[len(data)] = [run, generation, duration, fitness]
 
-        if verbose > 0:
-            print(f"finished ({fitness})")
-        if fitness >= config["fitness_threshold"]:
-            break
+            if verbose > 0:
+                print(f"finished ({fitness})")
+            if fitness >= config["fitness_threshold"]:
+                break
     
-    data.to_csv(config["output_path"])
+    data.to_csv(config["output_path"] + config["config_name"] + ".csv")
     
 
 def main():
