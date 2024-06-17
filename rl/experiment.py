6992930661e2ec4f7e6cef9ca1838bf3296b8f08
Jasiek <jasiekk9@gmail.com>
2024-06-17 02:09:36 +0200

    Finish RL experiment

--- rl/experiment.py
+++ rl/experiment.py
@@ -1,38 +1,58 @@
+import json
+
 import gymnasium as gym
-import numpy as np
 import torch
-from stable_baselines3 import PPO, DQN
+from stable_baselines3 import DQN
 import stable_baselines3.common.utils
 from stable_baselines3.common.logger import configure
 
 
-def main():
-    stable_baselines3.common.utils.set_random_seed(0)
-    new_logger = configure('./rl/output', ["stdout", "csv"])
+def run_experiment(config_path: str = './rl/config.json', verbose: int = 0):
+    with open(config_path, 'r') as f:
+        config = json.load(f)
 
-    env = gym.make("CartPole-v1")
+    new_logger = configure(config["output_path"], ["stdout", "csv"])
+    stable_baselines3.common.utils.set_random_seed(config["seed"])
+    env = gym.make(config["env_name"])
     policy_kwargs = dict(
         activation_fn=torch.nn.ReLU,
-        net_arch=[8, 8]
+        net_arch=config["net_arch"]
     )
     model = DQN(
-        "MlpPolicy", env, 
+        "MlpPolicy", 
+        env, 
         seed=0,
         policy_kwargs=policy_kwargs,
-        exploration_initial_eps=0.2,
-        learning_rate=1e-3,
-        verbose=1)
-    
-    model.set_logger(new_logger)
-    model.learn(total_timesteps=200000, log_interval=10)
+        exploration_initial_eps=0.3,
+        exploration_final_eps=0.03,
+        learning_rate=3e-3,
+        verbose=1
+    )
 
-    env = gym.make("CartPole-v1", render_mode="human")
-    obs, info = env.reset()
+    model.set_logger(new_logger)
+    model.learn(total_timesteps=config["total_timesteps"], log_interval=10)
+
+    # evaluate deterministic
+    env = gym.make(config["env_name"])
+    obs, _ = env.reset()
+    total_reward = 0
+    evaluation_run = 0
     while True:
-        action, _states = model.predict(obs, deterministic=True)
-        obs, reward, terminated, truncated, info = env.step(action)
+        if evaluation_run == config["evaluation_runs"]:
+            break
+        action, _ = model.predict(obs, deterministic=True)
+        obs, reward, terminated, truncated, _ = env.step(action)
+        total_reward += reward
         if terminated or truncated:
-            obs, info = env.reset()
+            obs, _ = env.reset()
+            evaluation_run += 1
+    with open(config["output_path"] + '/rl_evaluation.txt', 'w') as f:
+        f.write(f"{total_reward/config['evaluation_runs']}")
+
+
+def main():
+    run_experiment()
+
 
 if __name__ == '__main__':
     main()
